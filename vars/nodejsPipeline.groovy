63945f38be59873085c37fa16fe2930eae2ef537
heman chandra <hemanchandra701@gmail.com>
2026-08-31 10:11:37 +0530

    jenkins

--- vars/nodejsPipeline.groovy
+++ vars/nodejsPipeline.groovy
@@ -5,77 +5,82 @@ def call(Map configMap) {
                 label 'AGENT-1'
             }
         }
-        environment { 
-            Course = 'Jenkins'
-            appVersion = ""
-            ACC_ID = "634758830486"
-            PROJECT = configMap.get("project")
-            COMPONENT = configMap.get("component")
+
+        environment {
+            COURSE = 'Jenkins'
+            ACC_ID = '634758830486'
+            PROJECT = "${configMap.get('project')}"
+            COMPONENT = "${configMap.get('component')}"
+            APP_VERSION = ''
         }
+
         options {
-            timeout(time: 60, unit: 'MINUTES') 
+            timeout(time: 60, unit: 'MINUTES')
             disableConcurrentBuilds()
         }
-        
+
         stages {
             stage('App Version') {
                 steps {
                     script {
                         def packageJson = readJSON file: 'package.json'
-                        appVersion = packageJson.version
-                        echo "AppVersion: ${appVersion}"
+                        env.APP_VERSION = packageJson.version
+                        echo "AppVersion: ${env.APP_VERSION}"
                     }
                 }
             }
+
             stage('npm Install') {
                 steps {
-                    script {
-                        sh """
-                            npm install
-                        """
-                    }
+                    sh 'npm install'
                 }
             }
+
             stage('Unit Test') {
                 steps {
-                    script {
+                    sh 'npm test'
+                }
+            }
+
+            stage('ECR') {
+                steps {
+                    withAWS(
+                        region: 'us-east-1',
+                        credentials: 'aws'
+                    ) {
                         sh """
-                            npm test
+                            aws ecr get-login-password --region us-east-1 | \
+                            docker login \
+                            --username AWS \
+                            --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
+
+                            docker build \
+                            -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${APP_VERSION} .
+
+                            docker push \
+                            ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${APP_VERSION}
                         """
                     }
                 }
             }
-            stage('ECR') {
-                steps {
-                    script {
-                        withAWS(region: 'us-east-1', credentials: 'aws') {
-                            sh """
-                                aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
-                                docker build -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion} .
-                                docker push ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
-                            """
-                        }
-                    }
-                }
-            }
-            
-            
-
         }
-        post { 
-            always { 
+
+        post {
+            always {
                 echo 'I will always say Hello again!'
                 cleanWs()
+            }
 
-            }
             success {
-                echo 'I will run if pipeline sucess'
+                echo 'I will run if pipeline succeeds'
             }
+
             failure {
-                echo 'I will run if pipeline failed'
+                echo 'I will run if pipeline fails'
             }
+
             aborted {
-                echo 'pipeline is aborted'
+                echo 'Pipeline is aborted'
             }
         }
     }
